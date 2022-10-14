5b47f805680349908ffd64583385cb05c8f15cb3
Miguel Beltran <m@beltran.work>
2022-10-14 14:30:51 +0200

    chore(deps): upgrade dependencies (#75)
    * chore(deps): upgrade dependencies
    
    * fix: json regenerated

--- lib/src/messages/raygun_breadcrumb_message.g.dart
+++ lib/src/messages/raygun_breadcrumb_message.g.dart
@@ -12,7 +12,7 @@ RaygunBreadcrumbMessage _$RaygunBreadcrumbMessageFromJson(
       message: json['message'] as String,
       category: json['category'] as String?,
       level:
-          _$enumDecodeNullable(_$RaygunBreadcrumbLevelEnumMap, json['level']) ??
+          $enumDecodeNullable(_$RaygunBreadcrumbLevelEnumMap, json['level']) ??
               RaygunBreadcrumbLevel.info,
       customData: json['customData'] as Map<String, dynamic>?,
       className: json['className'] as String?,
@@ -26,50 +26,13 @@ Map<String, dynamic> _$RaygunBreadcrumbMessageToJson(
     <String, dynamic>{
       'message': instance.message,
       'category': instance.category,
-      'level': _$RaygunBreadcrumbLevelEnumMap[instance.level],
+      'level': _$RaygunBreadcrumbLevelEnumMap[instance.level]!,
       'customData': instance.customData,
       'className': instance.className,
       'methodName': instance.methodName,
       'lineNumber': instance.lineNumber,
       'timestamp': instance.timestamp,
     };
-
-K _$enumDecode<K, V>(
-  Map<K, V> enumValues,
-  Object? source, {
-  K? unknownValue,
-}) {
-  if (source == null) {
-    throw ArgumentError(
-      'A value must be provided. Supported values: '
-      '${enumValues.values.join(', ')}',
-    );
-  }
-
-  return enumValues.entries.singleWhere(
-    (e) => e.value == source,
-    orElse: () {
-      if (unknownValue == null) {
-        throw ArgumentError(
-          '`$source` is not one of the supported values: '
-          '${enumValues.values.join(', ')}',
-        );
-      }
-      return MapEntry(unknownValue, enumValues.values.first);
-    },
-  ).key;
-}
-
-K? _$enumDecodeNullable<K, V>(
-  Map<K, V> enumValues,
-  dynamic source, {
-  K? unknownValue,
-}) {
-  if (source == null) {
-    return null;
-  }
-  return _$enumDecode<K, V>(enumValues, source, unknownValue: unknownValue);
-}
 
 const _$RaygunBreadcrumbLevelEnumMap = {
   RaygunBreadcrumbLevel.debug: 0,
