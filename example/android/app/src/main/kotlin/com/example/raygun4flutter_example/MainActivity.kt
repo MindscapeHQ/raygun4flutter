3adaf087ba75c0b3e2eb330175bbd8d932d915b7
Miguel Beltran <m@beltran.work>
2025-03-03 10:57:38 +0100

    recreate Android project

---
+++ example/android/app/src/main/kotlin/com/example/raygun4flutter_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.raygun4flutter_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- example/android/app/src/main/kotlin/com/raygun/raygun4flutter/example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.raygun.raygun4flutter.example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
