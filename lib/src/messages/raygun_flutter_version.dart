3794a4cbb6cd385d58177ada76937af9763d9c2d
Miguel Beltran <m@beltran.work>
2025-03-12 13:17:13 +0100

    feat: #233 FlutterVersion payload message data

---
+++ lib/src/messages/raygun_flutter_version.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/services.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'raygun_flutter_version.g.dart';
+
+@JsonSerializable()
+class RaygunFlutterVersion {
+  String? version = FlutterVersion.version;
+  String? channel = FlutterVersion.channel;
+  String? gitUrl = FlutterVersion.gitUrl;
+  String? frameworkRevision = FlutterVersion.frameworkRevision;
+  String? engineRevision = FlutterVersion.engineRevision;
+  String? dartVersion = FlutterVersion.dartVersion;
+
+  RaygunFlutterVersion();
+
+  Map<String, dynamic> toJson() => _$RaygunFlutterVersionToJson(this);
+
+  factory RaygunFlutterVersion.fromJson(
+    Map<String, dynamic> json,
+  ) =>
+      _$RaygunFlutterVersionFromJson(json);
+}
