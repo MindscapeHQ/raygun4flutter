fc3a38c82250139c4b53e1a0fece6dc862f3b61f
Miguel Beltran <m@beltran.work>
2025-10-23 10:41:11 +0200

    fix: support wasm

--- lib/src/utils/device_info_util.dart
+++ lib/src/utils/device_info_util.dart
@@ -1,2 +1,3 @@
-export '_device_info_util_device.dart'
-    if (dart.library.html) '_device_info_util_web.dart';
+// Use web implementation for both web and wasm compilation targets
+export '_device_info_util_web.dart'
+    if (dart.library.io) '_device_info_util_device.dart';
