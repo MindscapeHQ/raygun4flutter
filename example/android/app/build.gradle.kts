c06d4a9d877b0105fe7c7dd5b3f75881e8d7d764
Miguel Beltran <m@beltran.work>
2025-10-23 11:01:44 +0200

    fix Android build

--- example/android/app/build.gradle.kts
+++ example/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.raygun4flutter_example"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "25.1.8937393" // Using a specific stable NDK version instead of flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
